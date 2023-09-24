b016ac698c282580c18470cc1cdc8822bd598dab
Fabio <fabio.ferreira@tecnofit.com.br>
2023-09-24 17:00:56 -0300

    solving pipe problems

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useAuthContext } from './hooks/useAuthContext';
+import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Create from './pages/create/Create';
 import Search from './pages/search/Search';
 import Recipe from './pages/recipe/Recipe';
 import Signup from './signup/Signup';
 import Login from './login/Login';
+import { useAuthContext } from './hooks/useAuthContext'; // Importe o hook de contexto de autenticação
+import { AuthContextProvider } from './caminho-para-seu-contexto-de-autenticacao'; // Importe seu contexto de autenticação
 import './App.css';
 
 function App() {
@@ -19,7 +21,7 @@ function App() {
         <Switch>
           <Route exact path='/'>
             {user ? <Home /> : <Redirect to="/login" />}
-          </Route> 
+          </Route>
           <Route path='/login'>
             {user ? <Redirect to="/" /> : <Login />}
           </Route>
@@ -33,7 +35,7 @@ function App() {
             <Recipe />
           </Route>
           <Route path='/signup'>
-            {user ? <Redirect to="/" /> : <Signup />} 
+            {user ? <Redirect to="/" /> : <Signup />}
           </Route>
         </Switch>
       </BrowserRouter>
@@ -42,3 +44,17 @@ function App() {
 }
 
 export default App;
+
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import { AuthContextProvider } from './caminho-para-seu-contexto-de-autenticacao'; // Importe o contexto de autenticação
+
+test('renders learn react link', () => {
+  render(
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  );
+});
